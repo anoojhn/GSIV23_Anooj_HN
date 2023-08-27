bcff7feec0138854101a1e9b8a9a05789a20ba8c
anoojhn <anoojhn@gmail.com>
2023-08-27 09:45:29 +0530

    E2E test added

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,51 +1,31 @@
-import { faker } from "@faker-js/faker";
-
-describe("smoke tests", () => {
-  afterEach(() => {
-    cy.cleanupUser();
+describe("List", () => {
+  beforeEach(() => {
+    cy.visit("/");
   });
 
-  it("should allow you to register and login", () => {
-    const loginForm = {
-      email: `${faker.internet.userName()}@example.com`,
-      password: faker.internet.password(),
-    };
+  it("renders movie cards", () => {
+    cy.get('[data-testid="movie-card"]').should("be.visible");
+    cy.get('[data-testid="movie-card"]').first().click();
+    cy.wait(1000);
+    cy.url().should("include", "/movies/");
+    cy.get(".text-2xl").should("be.visible");
 
-    cy.then(() => ({ email: loginForm.email })).as("user");
-
-    cy.visitAndCheck("/");
-
-    cy.findByRole("link", { name: /sign up/i }).click();
-
-    cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
-    cy.findByLabelText(/password/i).type(loginForm.password);
-    cy.findByRole("button", { name: /create account/i }).click();
-
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByRole("button", { name: /logout/i }).click();
-    cy.findByRole("link", { name: /log in/i });
+    cy.get('[data-testid="home"]').click();
+    cy.wait(1000);
+    cy.url().should("include", "/");
   });
 
-  it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
-    };
-    cy.login();
+  it("filters movies by search text", () => {
+    cy.get('[data-testid="search-input"]').type("Avengers");
 
-    cy.visitAndCheck("/");
+    cy.get('[data-testid="movie-card"]').should("have.length.above", 0);
+    cy.wait(2000);
+  });
 
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByText("No notes yet");
+  it("loads more movies on scroll", () => {
+    cy.scrollTo("bottom");
 
-    cy.findByRole("link", { name: /\+ new note/i }).click();
-
-    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
-    cy.findByRole("button", { name: /save/i }).click();
-
-    cy.findByRole("button", { name: /delete/i }).click();
-
-    cy.findByText("No notes yet");
+    cy.get('[data-testid="movie-card"]').should("have.length.above", 10);
+    cy.wait(2000);
   });
 });
