45696efd5764be40cb60450a2a206c4e32e91ab8
anoojhn <anoojhn@gmail.com>
2023-08-27 10:45:53 +0530

    Remix config update

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,17 @@
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.js",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
+
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  ...baseConfig,
   cacheDirectory: "./node_modules/.cache/remix",
   future: {
     v2_dev: true,
